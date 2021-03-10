a20de10f68489398abad7d8c2dcb5931c45353d2
Daniel <Daniel@LENOVO-SORA>
2021-03-10 19:15:59 +0100

    First Commit

---
+++ exampleLoop/src/loop/Loop.java
@@ -0,0 +1,18 @@
+package loop;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		
+		
+		
+		int number=1;
+		
+		while (number<5) {
+			System.out.println("I love java so many: "+number);
+			number++;
+		}
+
+	}
+
+}
