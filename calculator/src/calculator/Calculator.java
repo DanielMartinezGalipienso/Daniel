d8875ee65f1ca3b54591b740daea2a6cae0ef3c1
Daniel <Daniel@LENOVO-SORA>
2021-03-15 12:09:24 +0100

    Branch calculator

--- calculator/src/calculator/Calculator.java
+++ calculator/src/calculator/Calculator.java
@@ -26,43 +26,37 @@ public class Calculator {
 			String operation=reader.nextLine();
 						
 			if(operation.equals("quit")) {
+				System.out.println("Exiting...");
 				break;
 			}
-			if(operation.equals("three")) {
-				System.out.println("a → b");
-				System.out.println("c → x");
-				System.out.println("Enter the number of a, b, c");
-				int a=Integer.parseInt(reader.nextLine());
-				int b=Integer.parseInt(reader.nextLine());
-				int c=Integer.parseInt(reader.nextLine());
-				int mul=b*c;
-				double res=(double)mul/a;
-				System.out.println("x is "+res);
+			System.out.println("Enter the numbers");
+			
+			int first=Integer.parseInt(reader.nextLine());
+			int second=Integer.parseInt(reader.nextLine());
+			
+			if(operation.equals("three")){
+				// System.out.println("a → b");
+				// System.out.println("c → x");
+				// System.out.println("Enter the number of a, b, c");
+				int third=Integer.parseInt(reader.nextLine());
+				System.out.println("X is "+three(first, second, third));
+			}
+			else if(operation.equals("sum")) {
+				System.out.println("The sum of "+first+" + "+second+" = "+sum(first, second));
+			}else if(operation.equals("dif")) {
+				System.out.println("The difference of "+first+" - "+second+" = "+dif(first, second));
+			}else if(operation.equals("mul")) {
+				System.out.println("The multiplication of "+first+" x "+second+" = "+mul(first, second));
+			}else if(operation.equals("div")) {
+				System.out.println("The divide of "+first+" ÷ "+second+" = "+div(first, second));
+			}else if(operation.equals("rem")) {
+				System.out.println("The divide of "+first+" ÷ "+second+" = "+div(first, second)+" with a remainder of "+rem(first, second));
 			}else {
-				System.out.println("Enter the two numbers");
-				
-				int first=Integer.parseInt(reader.nextLine());
-				int second=Integer.parseInt(reader.nextLine());
-				
-				
-				if(operation.equals("sum")) {
-					System.out.println("The sum of "+first+" + "+second+" = "+sum(first, second));
-				}else if(operation.equals("dif")) {
-					System.out.println("The difference of "+first+" - "+second+" = "+dif(first, second));
-				}else if(operation.equals("mul")) {
-					System.out.println("The multiplication of "+first+" x "+second+" = "+mul(first, second));
-				}else if(operation.equals("div")) {
-					System.out.println("The divide of "+first+" ÷ "+second+" = "+div(first, second));
-				}else if(operation.equals("rem")) {
-					System.out.println("The divide of "+first+" ÷ "+second+" = "+div(first, second)+" with a remainder of "+rem(first, second));
-				}else {
-					System.out.println("Unknown command.");
-				}
+				System.out.println("Unknown command.");
 			}
 			
 		}
 
-		System.out.println("Exiting...");
 
 	}
 	
@@ -80,6 +74,9 @@ public class Calculator {
 	}
 	public static int rem(int first, int second) {
 		return first%second;
+	}
+	public static double three(int first, int second, int third) {
+		return ((first*third)/second);
 	}
 
 }
